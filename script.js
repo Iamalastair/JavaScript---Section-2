0fd8b143c33e99dd783a2941f0c5fa2d9d7ec173
Iamalastair <30842186+Iamalastair@users.noreply.github.com>
2017-08-13 22:54:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+//Lecture: variables
+/*
+var name = 'Alastair';
+console.log(name);
+
+var lastName = 'Hardie';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log = (fullAge);
+*/
+
+
+//Lecture: variables 2
+/*
+var name = 'Alastair';
+var age = '26';
+
+//console.log(name + age);
+//console.log(age+age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'ProjectManager';
+isMarried = false;
+
+console.log(name + 'is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'twenty six';
+job = 'developer';
+
+console.log(name + 'is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried + '.');
+*/
+
+//Lecture: operators
+/*
+var now = 2017;
+var birthYear = 2017 - 26;
+
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+
+var ageAlice = 26;
+var ageAlastair = 26;
+
+ageAlice = ageAlastair = (3 + 5) * 4 - 6;
+//ageAlice = ageAlastair = 26
+
+ageAlice++;
+ageAlastair *= 2;
+
+console.log(ageAlice);
+console.log(ageAlastair);
+*/
+
+//Lecture : if/else statements
